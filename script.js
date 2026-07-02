d78a400085b38ac47646e117a8535550e87014f6
iska <askarobo2002@gmail.com>
2026-07-02 11:10:43 +0800

    script.js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function toggleMenu(menuId){
 
-    let menu = document.getElementById(menuId);
+    const menu = document.getElementById(menuId);
 
-    let card = menu.closest('.card');
+    if(!menu) return;
+
+    const card = menu.closest('.card');
 
     document.querySelectorAll('.card').forEach(c => {
         if(c !== card){
@@ -10,45 +12,11 @@ function toggleMenu(menuId){
         }
     });
 
-    document.getElementById("lastUpdated").textContent =
-    "Last Updated: " + new Date().toLocaleDateString();
-
     if(menu.style.display === "block"){
         menu.style.display = "none";
         card.classList.remove('active');
-    } else {
+    }else{
         menu.style.display = "block";
         card.classList.add('active');
     }
-}
-function updateDateTime(){
-
-    const now = new Date();
-
-    document.getElementById("datetime").innerHTML =
-        now.toLocaleString();
-}
-
-setInterval(updateDateTime,1000);
-updateDateTime();
-
-/* Search Function */
-function searchCards(){
-
-    let input = document.getElementById("searchInput");
-    let filter = input.value.toLowerCase();
-
-    let cards = document.querySelectorAll(".card");
-
-    cards.forEach(function(card){
-
-        let text = card.textContent.toLowerCase();
-
-        if(text.includes(filter)){
-            card.style.display = "";
-        } else {
-            card.style.display = "none";
-        }
-
-    });
 }
